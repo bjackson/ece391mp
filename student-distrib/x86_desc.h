56bb242744a08c6559f8ea6d87efc847b38b15c0
Brett Jackson <brett@brettjackson.org>
2015-10-18 16:24:04 -0500

    loads gdt

--- student-distrib/x86_desc.h
+++ student-distrib/x86_desc.h
@@ -1,4 +1,4 @@
-/* x86_desc.h - Defines for various x86 descriptors, descriptor tables, 
+/* x86_desc.h - Defines for various x86 descriptors, descriptor tables,
  * and selectors
  * vim:ts=4 noexpandtab
  */
@@ -118,6 +118,7 @@ extern uint16_t ldt_desc;
 extern uint32_t ldt_size;
 extern seg_desc_t ldt_desc_ptr;
 extern seg_desc_t gdt_ptr;
+extern seg_desc_t gdt_desc_ptr;
 extern uint32_t ldt;
 
 extern uint32_t tss_size;
